c8db5e9dbbb243d5e5c4d970bf87163d598137c9
Maxime <maxime.letellier@bluewin.ch>
2017-09-07 19:41:36 +0200

    update of the project sql

--- subset/dataquality p3.py
+++ subset/dataquality p3.py
@@ -1,11 +1,13 @@
 # Import re : Regular Expression
 import re
 
+from string import maketrans
+
 osm_file = open("map.osm.xml", "r")
 
 phone_number_format_CH = re.compile(r'\S+\.?$', re.IGNORECASE)
 street_types = defaultdict(int)
-
+phone_format = defaultdict(int)
 # Audit Phone Numbers:
 
 def audit_country:
@@ -14,18 +16,28 @@ def audit_country:
     # files:
 
 def phone_cleaning(phone_raw):
+    '''
+    :param phone_raw: a swiss phone number raw number in either format.
+    :return: the phone number in the format +41 xx xxx xx xx
+    '''
     phone_raw.replace(" ", "")
     return "+41"+ " " +phone_raw[-9:-7] + " " + phone_raw[-7:-4] + " " + phone_raw[-4:-2] + " " + phone_raw[-2:]
 
-def audit_street_type(Phone_number, Country):
+mapping = maketrans("0123456789", "x"*10)
+
+def audit_phone_type(Phone_number, Country = "CH"):
+
     # phone number format: +41 or 0 xx xxx xx xx
     # to finish
+    # https://stackoverflow.com/questions/30141233/replacing-the-integers-in-a-string-with-xs-without-error-handling
     if Country == "CH":
-        m = phone_number_format_CH.search(Phone_number)
-    if m:
-        street_type = m.group()
+        m = Phone_number.translate(mapping)
+        phone_format = m.group()
 
-        street_types[street_type] += 1
+        phone_format[phone_type] += 1
+
+        return phone_format
+
 
 # Audit_Street_type
 
@@ -46,6 +58,8 @@ def print_sorted_dict(d):
 def is_street_name(elem):
     return (elem.tag == "tag") and (elem.attrib['k'] == "addr:street")
 
+def is_phone_number(elem):
+    return (elem.tag == "tag")
 def audit():
     for event, elem in ET.iterparse(osm_file):
         if is_street_name(elem):
