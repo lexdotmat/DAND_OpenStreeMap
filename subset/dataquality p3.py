5d80e987cf0e498461511184f809155ba663ed6e
Maxime <maxime.letellier@bluewin.ch>
2017-09-10 18:16:08 +0200

    update of the project sql

--- subset/dataquality p3.py
+++ subset/dataquality p3.py
@@ -23,13 +23,15 @@ def phone_cleaning(phone_raw):
     phone_raw.replace(" ", "")
     return "+41"+ " " +phone_raw[-9:-7] + " " + phone_raw[-7:-4] + " " + phone_raw[-4:-2] + " " + phone_raw[-2:]
 
+# mapping from https://www.tutorialspoint.com/python/string_maketrans.htm
+# https://stackoverflow.com/questions/30141233/replacing-the-integers-in-a-string-with-xs-without-error-handling
 mapping = maketrans("0123456789", "x"*10)
 
 def audit_phone_type(Phone_number, Country = "CH"):
 
     # phone number format: +41 or 0 xx xxx xx xx
     # to finish
-    # https://stackoverflow.com/questions/30141233/replacing-the-integers-in-a-string-with-xs-without-error-handling
+
     if Country == "CH":
         m = Phone_number.translate(mapping)
         phone_format = m.group()
